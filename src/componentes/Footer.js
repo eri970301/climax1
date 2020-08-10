609111f870b287b8ecb1f0f2757d5e35cc340ffb
eri970301 <erika970301@live.com>
2020-08-09 21:05:28 -0500

    second commit

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faGithubAlt,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    return (
-        <div></div>
-    )
+  return (
+    <footer className="bg-blue-900 pt-3">
+        <div className="justify-between">
+            <div className="mb-5 py-3 text-center">
+              <a href="https://www.facebook.com/fckHxC" target="_blank">
+                <FontAwesomeIcon
+                  className="icon_footer"
+                  icon={faFacebookF}
+                ></FontAwesomeIcon>
+              </a>
+              <a href="https://www.linkedin.com/in/erika-otalvaro-zuleta-a96365139/" target="_blank">
+                <FontAwesomeIcon
+                  className="icon_footer"
+                  icon={faLinkedinIn}
+                ></FontAwesomeIcon>
+              </a>
+              <a href="https://github.com/eri970301" target="_blank">
+                <FontAwesomeIcon
+                  className="icon_footer"
+                  icon={faGithubAlt}
+                ></FontAwesomeIcon>
+              </a>
+            </div>
+          </div>
+      <div className="text-center text-white mt-0 py-3">
+        © 2020 Copyright:
+        <a href="https://www.linkedin.com/in/erika-otalvaro-zuleta-a96365139/"> Erika Otalvaro Z.</a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/componentes/Semana.js
+++ src/componentes/Semana.js
@@ -11,40 +11,40 @@ class Semanal extends Component {
         return ( 
             <div className="text-center">
             <div className="daily">
-              <p className="">{this.props.dia1}</p>
+              <p>{this.props.dia1}</p>
               <h1>{this.props.descripciond1}</h1>
-              <p className="">
+              <p>
                 <FontAwesomeIcon icon={faCloudSun}></FontAwesomeIcon>Max.
                 temperature: {this.props.tempmaxd1}
               </p>
-              <p className="">Min. temperature: {this.props.tempmind1}</p>
+              <p>Min. temperature: {this.props.tempmind1}</p>
             </div>
-            <div className=" daily">
-              <p className="">{this.props.dia2}</p>
+            <div className="daily">
+              <p>{this.props.dia2}</p>
               <h1>{this.props.descripciond2}</h1>
-              <p className="">
+              <p>
                 <FontAwesomeIcon icon={faCloudRain}></FontAwesomeIcon>Max.
                 temperature: {this.props.tempmaxd2}
               </p>
-              <p className="">Min. temperature: {this.props.tempmind2}</p>
+              <p>Min. temperature: {this.props.tempmind2}</p>
             </div>
-            <div className=" daily">
-              <p className="">{this.props.dia3}</p>
+            <div className="daily">
+              <p>{this.props.dia3}</p>
               <h1>{this.props.descripciond3}</h1>
-              <p className="">
+              <p>
                 <FontAwesomeIcon icon={faCloudRain}></FontAwesomeIcon>Max.
                 temperature: {this.props.tempmaxd3}
               </p>
-              <p className="">Min. temperature: {this.props.tempmind3}</p>
+              <p>Min. temperature: {this.props.tempmind3}</p>
             </div>
-            <div className=" daily">
-              <p className="">{this.props.dia4}</p>
+            <div className="daily">
+              <p>{this.props.dia4}</p>
               <h1>{this.props.descripciond4}</h1>
-              <p className="">
+              <p>
                 <FontAwesomeIcon icon={faCloudSun}></FontAwesomeIcon>Max.
                 temperature: {this.props.tempmaxd4}
               </p>
-              <p className="">Min. temperature: {this.props.tempmind4}</p>
+              <p>Min. temperature: {this.props.tempmind4}</p>
             </div>
           </div>
          );
